277edbe68f5baeb8d2e5eb91037534864b65b277
math-otter <yeonung0315@gmail.com>
2024-12-16 21:43:48 +0900

    Update autocorr_practice.py

--- skill_usage_time_analysis/0_trash/autocorr_practice.py
+++ skill_usage_time_analysis/0_trash/autocorr_practice.py
@@ -2,26 +2,26 @@ import numpy as np
 
 # 1. 정의
 x = np.array(['x(0)', 'x(1)', 'x(2)'])
-n = len(x)
-for tau in range(n):
+p = len(x)
+for tau in range(p):
     r = f'Rxx({tau}) = '
-    for t in range(n - tau):
+    for t in range(p - tau):
         r = r + f'+ {x[t + tau]}*{x[t]} '
     print(r)
 
 # 2. 함수 만들기
 def autocorr(x):
-    n = len(x)
+    p = len(x)
     autocorr = []
-    for tau in range(n):
+    for tau in range(p):
         r = 0
-        for t in range(n - tau):
+        for t in range(p - tau):
             r += x[t + tau]*x[t]
         autocorr.append(r)
     return autocorr
 
 # 3. 간결화
 def autocorr(x):
-    n = len(x)
-    return [np.sum(x[:n - tau] * x[tau:]) for tau in range(n)]
+    p = len(x)
+    return [np.sum(x[:p - tau] * x[tau:]) for tau in range(p)]
 
